597326e6b7d90c5ce6a398dc8de739edc27a1a69
regent-developer <hu-yingdong@outlook.com>
2020-09-26 00:07:50 +0800

    add

--- golang/数据结构/循环链表/circleLinkedList.go
+++ golang/数据结构/循环链表/circleLinkedList.go
@@ -24,22 +24,64 @@ type List struct{
 
 // 初始化链表
 func (this *List) Init() {
-
+	list := *this
+	list.size = 0
+	list.headNode = nil
 }
 
 // 添加数据到链表尾部
-func (this *List) Append(data Object) bool {
-	return false
+func (this *List) Append(data Object) {
+	// 生成节点
+	node := new(Node)
+
+	node.Data = data
+
+	if this.GetSize() == 0 {
+		// 没有节点时
+		(*this).headNode = node
+	} else {
+		item := this.GetHead()
+
+		for ; (*item).Next != this.GetHead(); item = (*item).Next {}
+
+		(*item).Next = node
+	}
+
+	(*node).Next = (*this).headNode
+	(*this).size++
 }
 
 // 指定节点后插入
 func (this *List) Insert(node *Node, data Object) bool {
-	return false
+	if node == nil {
+		return false
+	}
+
+	item := new(Node)
+	(*item).Data = data
+
+	(*node).Next = (*node).GetNext()
+	(*node).Next = item
+
+	this.size++
+
+	return true
 }
 
 // 删除节点
 func (this *List) Remove(node *Node) Object {
-	return nil
+	if node == nil {
+		return nil
+	}
+
+	item := (*this).GetHead()
+
+	for ; (*item).Next != this.GetHead(); item = (*item).Next {}
+
+	(*item).Next = (*node).Next
+	(*this).GetSize()--
+
+	return (*node).GetData()
 }
 
 // 获取头节点
