ff9b37ae8cd524f157d05a3db22363bf3c974f97
regent-developer <68065963+regent-developer@users.noreply.github.com>
2023-10-06 20:37:53 +0800

    多项式分布（离散型） add

---
+++ python/Multinomial_distribution_(discrete).py
@@ -0,0 +1,46 @@
+# 多项式分布（离散型）
+# 范畴分布是多项式分布（Multinomial distribution）的一个特例，它与范畴分布的关系就像伯努利分布与二项分布之间的关系。
+import numpy as np
+from matplotlib import pyplot as plt
+
+import operator as op
+from functools import reduce
+
+def factorial(n):
+    return reduce(op.mul, range(1, n + 1), 1)
+
+def const(n, a, b, c):
+    """
+        return n! / a! b! c!, where a+b+c == n
+    """
+    assert  a + b + c == n
+
+    numer = factorial(n)
+    denom = factorial(a) * factorial(b) * factorial(c)
+    return numer / denom
+
+def multinomial(n):
+    """
+    :param x : list, sum(x) should be `n`
+    :param n : number of trial
+    :param p: list, sum(p) should be `1`
+    """
+    # get all a,b,c where a+b+c == n, a<b<c
+    ls = []
+    for i in range(1, n + 1):
+        for j in range(i, n + 1):
+            for k in range(j, n + 1):
+                if i + j + k == n:
+                    ls.append([i, j, k])
+
+    y = [const(n, l[0], l[1], l[2]) for l in ls]
+    x = np.arange(len(y))
+    return x, y, np.mean(y), np.std(y)
+
+for n_experiment in [20, 21, 22]:
+    x, y, u, s = multinomial(n_experiment)
+    plt.scatter(x, y, label=r'$trial=%d$' % (n_experiment))
+
+plt.legend()
+plt.savefig('graph/multinomial.png')
+plt.show()
