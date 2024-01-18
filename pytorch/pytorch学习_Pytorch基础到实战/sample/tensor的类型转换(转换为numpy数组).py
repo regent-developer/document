1bcbb9b1c2a31b571891a87c7a0cb9e2c8dc8a6f
regent-developer <68065963+regent-developer@users.noreply.github.com>
2024-01-18 09:05:37 +0800

    tensor的类型转换(转换为numpy数组) add

---
+++ pytorch/pytorch学习_Pytorch基础到实战/sample/tensor的类型转换(转换为numpy数组).py
@@ -0,0 +1,19 @@
+import torch
+
+# 使用张量对象中numpy函数进行转换
+data_tensor = torch.tensor([2, 3, 4])
+data_numpy = data_tensor.numpy()
+print('使用张量对象中numpy函数进行转换')
+print(data_tensor)
+print(data_numpy)
+
+# 使用copy函数进行转换拷贝
+data_numpy = data_tensor.numpy()
+print('使用copy函数进行转换拷贝')
+print(data_tensor)
+print(data_numpy)
+
+# 修改转换后数组，原数据不变
+data_numpy[0] = 100
+print(data_tensor)
+print(data_numpy)
