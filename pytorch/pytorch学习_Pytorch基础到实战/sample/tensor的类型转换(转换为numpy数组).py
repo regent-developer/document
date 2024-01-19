6de277bb4d6812538895f439334669ad4e6d7bf0
regent-developer <68065963+regent-developer@users.noreply.github.com>
2024-01-19 09:06:55 +0800

    update

--- pytorch/pytorch学习_Pytorch基础到实战/sample/tensor的类型转换(转换为numpy数组).py
+++ pytorch/pytorch学习_Pytorch基础到实战/sample/tensor的类型转换(转换为numpy数组).py
@@ -7,13 +7,22 @@ print('使用张量对象中numpy函数进行转换')
 print(data_tensor)
 print(data_numpy)
 
+# 修改转换后数组，原数据改变
+data_numpy[0] = 100
+print(data_tensor)
+print(data_numpy)
+
 # 使用copy函数进行转换拷贝
-data_numpy = data_tensor.numpy()
+data_copy = torch.tensor([2, 3, 4])
+data_copy.copy_(data_tensor)
+
+data_numpy  = data_copy.numpy()
+
 print('使用copy函数进行转换拷贝')
 print(data_tensor)
 print(data_numpy)
 
 # 修改转换后数组，原数据不变
-data_numpy[0] = 100
+data_numpy[0] = 1
 print(data_tensor)
 print(data_numpy)
