18238ca6887b7d50f73c01a6574729e103e4a271
regent-developer <68065963+regent-developer@users.noreply.github.com>
2024-01-24 09:11:08 +0800

    update

--- pytorch/pytorch学习_Pytorch基础到实战/sample/tensor的索引(列表索引).py
+++ pytorch/pytorch学习_Pytorch基础到实战/sample/tensor的索引(列表索引).py
@@ -1,8 +1,8 @@
 import torch
 
-data = torch.randint(0, 10, [4, 5])
+data = torch.randint(0, 10, [4, 5, 3])
 print(data)
 
-print(data[[0,1], [1,1]])
+print(data[[0,1], [1,1], [1,1]])
 
 print(data[[[0], [1]], [1,2]])
